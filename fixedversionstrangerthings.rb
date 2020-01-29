950665a6e82bd913195995485921928856f0c6ec
austinfulks <54602734+austinfulks@users.noreply.github.com>
2020-01-29 13:40:33 -0800

    Add files via upload

---
+++ fixedversionstrangerthings.rb
@@ -0,0 +1,22 @@
+# Welcome to Sonic Pi v3.1
+# Welcome to Sonic Pi v3.1
+live_loop :tryhard do
+  with_fx :echo do
+    play :C4
+    sleep 0.25
+    play :E4
+    sleep 0.25
+    play :G4
+    sleep 0.25
+    play :B4
+    sleep 0.25
+    play :C5
+    sleep 0.25
+    play :B4
+    sleep 0.25
+    play :G4
+    sleep 0.25
+    play :E4
+    sleep 0.25
+  end
+end
